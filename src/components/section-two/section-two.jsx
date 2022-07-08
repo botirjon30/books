2f1f0549d8a2cabcb9e19456c3309589402bb7e7
turgunov24 <learningplatforms.turgunov@gmail.com>
2022-07-08 15:00:06 +0500

    hi

--- src/components/section-two/section-two.jsx
+++ src/components/section-two/section-two.jsx
@@ -1,24 +1,32 @@
-import React, { useContext } from 'react';
-import { Context } from '../contex';
-
-
+import React, { useContext } from "react";
+import { Context } from "../contex";
+import Navbar from "../navbar/navbar";
 
 function SectionTwo(props) {
-    console.log(props);
-    const { orders } = useContext(Context);
-    return ( 
-        <div>
-            {
-                orders && orders.map((item)=>(
-                    <div className='bg-white' key={'aliffdf'}>
-                       <h1>{item.name}</h1>
-                       <img src={item.images} alt="" />
-                       <h1>{item.desc}</h1>
-                    </div>                
-                ))
-            }
-        </div> 
-    );
+  console.log(props);
+  const { orders } = useContext(Context);
+  return (
+    <div className="flex flex-col w-full h-screen">
+        <Navbar />
+      {orders &&
+        orders.map((item) => (
+            <div className="flex w-full border">
+                <div className="flex flex-col items-center border w-1/2 border-gray-400">
+                    <img src={item.images} className="h-5/6 w-full" />
+                    <div className="w-full py-2 px-5 flex flex-col items-start gap-3">
+                        <small>Tavallud sanasi va O'lgan sanasi</small>
+                        <h1 className="font-satisfy text-5xl">{item.year}</h1>
+                        <small>Toshkent,Uzbekistan</small>
+                    </div>
+                </div>
+            </div>
+        ))}
+    </div>
+  );
 }
 
 export default SectionTwo;
+{/* <div className="bg-white" key={"aliffdf"}>
+<h1>{item.name}</h1>
+<img src={item.images} alt="" />
+</div> */}
